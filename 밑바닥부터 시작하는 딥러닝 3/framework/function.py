2b64e3ed2f14b8e2deb2786deaa91becc9696882
Asanobm <asanobm@outlook.com>
2024-03-26 07:42:41 +0900

    Implement backward method in Square and Exp classes

--- 밑바닥부터 시작하는 딥러닝 3/framework/function.py
+++ 밑바닥부터 시작하는 딥러닝 3/framework/function.py
@@ -9,9 +9,13 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input  # 입력 변수를 기억(보관)한다.
         return output
 
     def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
         raise NotImplementedError()
 
 
@@ -19,12 +23,30 @@ class Square(Function):
     """입력값을 제곱한다."""
 
     def forward(self, x):
+        """ $$ y = x^2 $$
+        """
         return x ** 2
+
+    def backward(self, gy):
+        """$$\frac{dy}{dx} = 2x$$
+        """
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 
 
 class Exp(Function):
-    """$y=e^x$를 계산한다.
+    """지수 함수를 계산한다.
     """
 
     def forward(self, x):
+        """$$y = \exp(x)$$
+        """
         return np.exp(x)
+
+    def backward(self, gy):
+        """$$\frac{dy}{dx} = \exp(x)$$
+        """
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
