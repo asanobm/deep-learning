f6d363ce0875f4f8a7c2b3d85f723b1366d3b6af
Asanobm <asanobm@outlook.com>
2024-03-26 07:52:16 +0900

    Add set_creator method to Variable class

--- 밑바닥부터 시작하는 딥러닝 3/framework/variable.py
+++ 밑바닥부터 시작하는 딥러닝 3/framework/variable.py
@@ -2,3 +2,7 @@ class Variable:
     def __init__(self, data):
         self.data = data  # 데이터 ndarray
         self.grad = None  # 미분값을 저장하는 변수 ndarray
+        self.creator = None  # 변수의 창조자(creator)를 기억하는 변수
+
+    def set_creator(self, func):
+        self.creator = func
