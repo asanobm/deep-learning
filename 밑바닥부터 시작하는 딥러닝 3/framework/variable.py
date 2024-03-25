a895962f9aa31c7ad139443c9f58e509d810e2c3
Asanobm <asanobm@outlook.com>
2024-03-26 08:05:14 +0900

    Refactor backward method to use iteration instead of recursion

--- 밑바닥부터 시작하는 딥러닝 3/framework/variable.py
+++ 밑바닥부터 시작하는 딥러닝 3/framework/variable.py
@@ -7,9 +7,21 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
+    # def backward(self):
+    #     f = self.creator
+    #     if f is not None:
+    #         x = f.input  # 함수의 입력을 가져온다.
+    #         x.grad = f.backward(self.grad)  # 함수의 backward 메서드를 호출한다.
+    #         x.backward()  # 하나 앞 변수의 backward 메서드를 호출한다.
+
     def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input  # 함수의 입력을 가져온다.
-            x.grad = f.backward(self.grad)  # 함수의 backward 메서드를 호출한다.
-            x.backward()  # 하나 앞 변수의 backward 메서드를 호출한다.
+        """재귀에서 반복문으로 변경"""
+        functions = [self.creator]
+
+        while functions:
+            f = functions.pop()  # 함수를 가져온다.
+            x, y = f.input, f.output  # 함수의 입력과 출력을 가져온다.
+            x.grad = f.backward(y.grad)  # backward 메서드를 호출한다.
+
+            if x.creator is not None:
+                functions.append(x.creator)  # 하나 앞의 함수를 리스트에 추가한다.
